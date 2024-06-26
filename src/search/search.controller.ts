1244c901bff20eea0ea1d72633537b8142558e98
Lucas Ocon <lucas_masterclas@hotmail.com>
2024-06-25 23:50:38 -0300

    Remove arg

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,11 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { SearchService } from './search.service';
-import { PrismaService } from 'src/prisma.service';
 import { Employee as EmployeeModel } from '@prisma/client';
 
 @Controller({})
 export class SearchController {
-  constructor(private prisma: PrismaService, private readonly searchService: SearchService) {}
+  constructor(private readonly searchService: SearchService) {}
 
   @Post('/search')
   async createSearch(@Body() search: any): Promise<EmployeeModel[]> {
