769576be35a0591d26414bb22bf71aa95d4c41a2
Lucas Ocon <lucas_masterclas@hotmail.com>
2024-06-25 23:45:15 -0300

    Add type

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { PrismaService } from 'src/prisma.service';
+import { Employee as EmployeeModel } from '@prisma/client';
 
 @Controller({})
 export class SearchController {
   constructor(private prisma: PrismaService, private readonly searchService: SearchService) {}
 
   @Post('/search')
-  async createSearch(@Body() search: any) {
+  async createSearch(@Body() search: any): Promise<EmployeeModel[]> {
     return await this.searchService.getEmployees(search);
   }
 }
